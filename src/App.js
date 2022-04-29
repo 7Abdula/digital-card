fb4c6ad66cf866702bf610e1bc7b9dd9f75a759d
Abdula Alqahtani <7Abdula@gmail.com>
2022-04-30 01:36:07 +0300

    Create Interests component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Info from './components/Info';
 import About from './components/About';
+import Interests from './components/Interests';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className="max-w-[317px] bg-[#1A1B21] rounded-[10px]">
         <Info />
         <About />
+        <Interests />
       </div>
     </div>
   )
---
+++ src/components/Interests.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Interests = () => {
+  return (
+    <div className="mt-[24px] px-[36px]">
+      <h1 className="text-[#F5F5F5] font-bold text-base leading-6">Interests</h1>
+      <p className="text-[10.24px] leading-[15.36px] text-[#DCDCDC] mt-[5px]">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </div>
+  )
+}
+
+export default Interests;
