13916de764d9c343139aee434fcb5218663645c4
Abdula Alqahtani <7Abdula@gmail.com>
2022-04-30 01:31:04 +0300

    Create About component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Info from './components/Info';
+import About from './components/About';
 
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-[#23252C] font-inter">
-      <div className="min-w-[317px] bg-[#1A1B21] rounded-[10px]">
+      <div className="max-w-[317px] bg-[#1A1B21] rounded-[10px]">
         <Info />
-
+        <About />
       </div>
     </div>
   )
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mt-[33px] px-[36px]">
+      <h1 className="text-[#F5F5F5] font-bold text-base leading-6">About</h1>
+      <p className="text-[10.24px] leading-[15.36px] text-[#DCDCDC] mt-[5px]">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+    </div>
+  )
+}
+
+export default About;
